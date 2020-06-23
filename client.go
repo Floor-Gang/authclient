c04419dde0ab7ad039f59bb3ba9ff96fb1d52dc7
Dylan Hackworth <dhpf@pm.me>
2020-06-22 21:45:46 -0500

    Initial Commit

---
+++ client.go
@@ -0,0 +1,41 @@
+package auth_client
+
+import (
+	"net/rpc"
+)
+
+type AuthArgs struct{ MemberID string }
+
+type AuthResponse struct {
+	Role    string
+	IsAdmin bool
+}
+
+type AuthClient struct {
+	client *rpc.Client
+}
+
+func GetClient(address string) (AuthClient, error) {
+	client, err := rpc.DialHTTP("tcp", address)
+
+	if err != nil {
+		return AuthClient{}, err
+	}
+
+	return AuthClient{
+		client: client,
+	}, nil
+}
+
+func (ac AuthClient) Auth(memberID string) (AuthResponse, error) {
+	response := AuthResponse{}
+	args := AuthArgs{MemberID: memberID}
+
+	err := ac.client.Call("AuthServer.Auth", args, &response)
+
+	if err != nil {
+		return response, err
+	} else {
+		return response, nil
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module auth_client
+
+go 1.14
