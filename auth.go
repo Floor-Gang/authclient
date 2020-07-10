14ca1e383b0bea9e752782fb3f0c61f76dab0cc5
Dylan Hackworth <dhpf@pm.me>
2020-07-09 20:09:44 -0500

    Added more auth methods

--- auth.go
+++ auth.go
@@ -7,6 +7,26 @@ type AuthResponse struct {
 	IsAdmin bool
 }
 
+func (ac AuthClient) CheckMember(memberID string) (bool, error) {
+	response, err := ac.Auth(memberID)
+
+	if err != nil {
+		return false, err
+	} else {
+		return response.IsAdmin, nil
+	}
+}
+
+func (ac AuthClient) GetRole(memberID string) (string, error) {
+	response, err := ac.Auth(memberID)
+
+	if err != nil {
+		return "", err
+	} else {
+		return response.Role, nil
+	}
+}
+
 func (ac AuthClient) Auth(memberID string) (AuthResponse, error) {
 	response := AuthResponse{}
 	args := AuthArgs{MemberID: memberID}
